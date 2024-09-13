61db3550233a5634da8e8f301338e51f2afffdeb
imedve01 <igor.medvedovsky@gmail.com>
2024-09-13 15:26:19 -0400

    .

--- starter/apps/blue-green/blue-green.sh
+++ starter/apps/blue-green/blue-green.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 set -e
+
+# Replace with real URL
+SVC_URL="172.20.204.114"
+
+kubectl apply -f index_green_html.yml
+kubectl apply -f green.yml
+
+sleep 10
+
+RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" "SVC_URL")
+
+if [ "$HTRESPONSE_CODE" -eq 200 ]; then
+  echo "Green deployment is OK"
+else
+  echo "Green deployment is not OK"
+  exit 1
+fi
--- starter/apps/canary/canary.sh
+++ starter/apps/canary/canary.sh
@@ -5,6 +5,10 @@ set -e
 # Replace with real URL
 CANARY_SVC_URL="172.20.204.114"
 
+kubectl apply -f canary-v2.yml
+
+sleep 10
+
 RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$CANARY_SVC_URL")
 
 if [ "$HTRESPONSE_CODE" -eq 200 ]; then
