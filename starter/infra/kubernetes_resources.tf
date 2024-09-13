a895025d78dcbd1ab0bdc8cd1d63bebb964eb3d1
imedve01 <igor.medvedovsky@gmail.com>
2024-09-13 18:19:50 -0400

    .

--- starter/infra/kubernetes_resources.tf
+++ starter/infra/kubernetes_resources.tf
@@ -60,17 +60,3 @@ resource "kubernetes_service" "green" {
     module.project_eks
   ]
 }
-
-resource "aws_route53_record" "green" {
-  zone_id = aws_route53_zone.private_dns.id
-  name    = "blue-green"
-  type    = "CNAME"
-  ttl     = "5"
-
-  weighted_routing_policy {
-    weight = 1
-  }
-
-  set_identifier = "green"
-  records        = [kubernetes_service.green.status.0.load_balancer.0.ingress.0.hostname] # https://github.com/hashicorp/terraform-provider-kubernetes/pull/1125
-}
