27109cc84f7ac62719e1cb5df4e390efd0ec0e3f
imedve01 <igor.medvedovsky@gmail.com>
2024-09-13 12:31:39 -0400

    add

---
+++ starter/apps/canary/canary.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+CANARY_SVC_URL="172.20.204.114"
+
+RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$CANARY_SVC_URL")
+
+if [ "$HTRESPONSE_CODE" -eq 200 ]; then
+  echo "Canary deployment is OK"
+else
+  echo "Canary deployment is not OK"
+  exit 1
+fi
